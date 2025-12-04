6e5e8e5aa76b46307dfb88a69977bdb4e568051c
skdutta-in <skdutta.ai@gmail.com>
2025-12-04 10:43:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -148,7 +148,7 @@ async function loadMini() {
     let data = await res.json();
 
     // if your JSON has levels, filter here, else random pick
-    let pick = data.sort(()=>0.5-Math.random()).slice(0,10);
+    let pick = data.sort(()=>0.5-Math.random()).slice(0,8);
 
     let out="<ol>";
     pick.forEach(q => out+=`<li>${q.replace("____","__________")}</li>`);
